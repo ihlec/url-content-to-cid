9f21395118afc8851e7033915713eaaf952315d4
Cornelius Ihle <ihle.cornelius@gmail.com>
2024-11-30 00:33:35 +0100

    first experiments with helia

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # url-content-to-cid
 Fetches a files from URLs and saves the CID
+
+## Get Started
+npm install
+
+npm start
+
+Input file: data.csv
+Output file: output.csv
+
+## Experimental
+Pinning and Provide currently removed
+
+## Tips
+tweak the pLimit value when running into rate limits
+files are stored in the node's memory (much RAM needed)
---
+++ get-cid-from-url.js
@@ -0,0 +1,71 @@
+import { createHeliaHTTP } from "@helia/http";
+import { unixfs } from "@helia/unixfs";
+import * as nodefs from "node:fs";
+import fetch from "node-fetch";
+import { parse } from "csv-parse";
+import pLimit from "p-limit";
+
+const helia = await createHeliaHTTP();
+let heliafs = unixfs(helia);
+// prevent rate limit errors with pLimit
+const limit = pLimit(10);
+const outputciddata = 'document, cid';
+nodefs.writeFile('output.csv', outputciddata, (err) => {
+    if (err) {
+        console.error('Error writing file:', err);
+    } else {
+        console.log('File written successfully!');
+    }
+});
+
+async function downloadPDFandCalcCID(url, filename) {
+  const r = await fetch(url);
+  const b = await r.blob();
+  const ab = await b.arrayBuffer();
+
+  // add a file and wrap in a directory
+  const c = await heliafs.addFile({
+    path: "./" + filename,
+    content: new Uint8Array(ab),
+    mode: 0x755,
+    mtime: {
+      secs: 10n,
+      nsecs: 0,
+    },
+  });
+  console.info("CID: ", c);
+  return c;
+}
+
+async function processCSV(csvfilename) {
+  try {
+    nodefs
+      .createReadStream(csvfilename)
+      .pipe(parse({ columns: true }))
+      .on("data", async (row) => {
+        // EVENT IS SPAMMING ==> set limiter
+        // console.log(row);
+        const url = row["source"];
+        const filename = row["document"];
+        //console.info("URL: ", url);
+        const cid = limit(() => downloadPDFandCalcCID(url, filename));
+
+        // pin file to node
+        //await helia.pins.add(cid)
+
+        // advertise the file on IPFS
+        //await helia.routing.provide(cid);
+
+        // save to output file
+        nodefs.appendFile('output.csv', '\n' + filename  + ',' + await cid, (err) => {
+          });
+
+      });
+  } catch (error) {
+    console.error("CSV read error", error);
+  }
+}
+
+processCSV("data.csv");
+
+
